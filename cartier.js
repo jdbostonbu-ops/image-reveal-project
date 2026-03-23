009513030c9e515624a12d6790c8612c8bffa02e
Jacqueline <jack@Jacquelines-MacBook-Air.local>
2026-03-22 20:12:07 -0400

    Updates

--- cartier.js
+++
@@ -1,8 +0,0 @@
-// Inside your useGSAP loop, after the mask animations:
-tl.to(container.querySelector(".side-list-container"), {
-    opacity: 1,
-    y: 0,
-    duration: 0.8,
-    ease: "power2.out"
-}, "-=0.5"); 
-
